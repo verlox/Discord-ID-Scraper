e150072e1e772882e2e556c3111fe8e37c69e627
verlox <verlox@riseup.net>
2021-08-30 01:33:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Discord ID Scraper
+
+## About
+This tool scrapes Discord user IDs from a guild specified. This is a selfbot, so there is your warning.
+
+## Requirements
+* Windows 10 PC
+* .NET Framework 4.7.2
+
+## Instructions
+1. Download release
+2. Run release
+3. Follow steps in console
---
+++ ScrapeGuildMembers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veylib.CLIUI;
+using Discord.Gateway;
+using System.Drawing;
+using System.IO;
+
+namespace ScrapeGuildMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Core core = Core.GetInstance();
+            core.Start(new StartupProperties { });
+
+            System.Threading.Thread.Sleep(500);
+
+            var token = core.ReadLine("Token? ");
+            var guildid = ulong.Parse(core.ReadLine("Guild ID? "));
+            var count = int.Parse(core.ReadLine("How many users to scrape? "));
+
+            var client = new DiscordSocketClient();
+            client.Login(token);
+            client.OnLoggedIn += (_, _2) =>
+            {
+                var random = true;
+
+                List<string> ids = new List<string>();
+                Random rand = new Random();
+
+                SocketGuild guild = client.GetCachedGuild(guildid);
+
+                core.WriteLine(Color.Yellow, "Fetching guild members, this will take a while");
+                var members = guild.GetMembers();
+                for (var x = 0; x < count; x++)
+                {
+                    int randIndex = rand.Next(members.Count - 1);
+                    if (!ids.Contains(members[randIndex].User.Id.ToString()))
+                    {
+                        ids.Add(members[randIndex].User.Id.ToString());
+                        core.WriteLine(Color.Lime, $"Added {members[randIndex].User.Username}#{members[randIndex].User.Discriminator} to list");
+                    }
+                }
+
+                File.WriteAllText("scraped-ids.txt", string.Join("\n", ids));
+            };
+        }
+    }
+}
